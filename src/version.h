726300d25b9d66616f7080daec8fe3cd87fdfbb6
André <andre_miguel_costa@hotmail.com>
2025-06-06 15:10:29 +0200

    chore: release v1.0.0

--- src/version.h
+++ src/version.h
@@ -2,8 +2,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION_MAJOR 0
-#define VERSION_MINOR 2
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
 #define VERSION_PATCH 0
 
 #define TOSTRING(x)   STRINGIFY(x)
